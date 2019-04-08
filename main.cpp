86610c18522b8d860f7d775d5a2a59b4af1572c8
yusuke0626 <m1716@s.akashi.ac.jp>
2019-04-09 06:19:08 +0900

    solenoid

--- main.cpp
+++ main.cpp
@@ -24,8 +24,7 @@ int main(void){
 	constexpr short LEFT_T_ARM = 3;
 	constexpr short TOWEL_SOLENOID = 4;
 	//constexpr short STICK_MAX_VALUE = 250;
-	constexpr short HANGER_LEFT_SOLENOID = 3;
-	constexpr short HANGER_RIGHT_SOLENOID = 3;
+	constexpr short HANGER_SOLENOID = 6;
 	//constexpr short POWER_WINDOW_MOTOR_NUM = 4;
 
 	double regulation = 0.3;
@@ -51,9 +50,13 @@ int main(void){
 	gpioSetMode(16,PI_PUD_UP);
 
 	bool hanger_flag = true;
+
 	bool box_flag = true;
+
 	bool coat_flag = true;
+
 	bool towel_flag = true;
+
 	bool moving_flag = false;
 
 	UPDATELOOP(controller, !(controller.button(RPDS3::START) && controller.button(RPDS3::RIGHT))){
@@ -128,11 +131,11 @@ int main(void){
 		//ハンガー昇降機構
 		if(controller.press(RPDS3::SQUARE)){
 			if(hanger_flag == true){
-				ms.send(MECHANISM_MDD_NUM,6,1);
-				ms.send(MECHANISM_MDD_NUM,6,2);
+				ms.send(MECHANISM_MDD_NUM,HUNGER_SOLENOID,1);
+				ms.send(MECHANISM_MDD_NUM,HUNGER_SOLENOID,2);
 				hanger_flag = false;
 			}else{
-				ms.send(MECHANISM_MDD_NUM,6,0);
+				ms.send(MECHANISM_MDD_NUM,HUNGER_SOLENOID,0);
 				hanger_flag = true;
 			}
 			std::cout << "sqrt" << std::endl;
