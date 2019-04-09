d6198ac293fcbf9828eb319586a78130aeca0f02
yusuke0626 <m1716@s.akashi.ac.jp>
2019-04-09 15:43:16 +0900

    エアシリンダ改善されていません

--- limit_test.cpp
+++ limit_test.cpp
@@ -4,6 +4,8 @@
 
 int main(){
 	std::cout << "Ctrl + C でプログラム終了します" << std::endl;
+	std::cout << "通電中には０が、スイッチが押されているときは１が表示され続けます" << std::endl;
+	std::cout << "スイッチから手を離してください" << std::endl;
 	std::cout << "確認したいピンの番号を入力してください" << std::endl;
 
 	int pin;
@@ -15,7 +17,7 @@ int main(){
 
 	int x;
 	x = gpioRead(pin);
-
+	
 	if( x == 1){
 		std::cout << "通電していません。配線及びスイッチの確認をしてください。" << std::endl;
 		exit(0);
