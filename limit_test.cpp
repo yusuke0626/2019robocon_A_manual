f24e75d951b0b9f765bb11b7678630193d9e6d4e
yusuke0626 <m1716@s.akashi.ac.jp>
2019-04-10 20:31:43 +0900

    limit checker

--- limit_test.cpp
+++ limit_test.cpp
@@ -3,11 +3,13 @@
 #include<stdlib.h>
 
 int main(){
+	std::cout << "リミットスイッチチェッカー起動しました" << std::endl;
 	std::cout << "Ctrl + C でプログラム終了します" << std::endl;
 	std::cout << "通電中には０が、スイッチが押されているときは１が表示され続けます" << std::endl;
 	std::cout << "スイッチから手を離してください" << std::endl;
 	std::cout << "確認したいピンの番号を入力してください" << std::endl;
-
+	std::cout << "M-2 右上　→　12 右下　→　16 左上　→  11 左下　→　22" << std::endl;
+	std::cout << "M-3 Y軸前　→　26　Y軸後ろ　→　19　Z軸上　→　9　Z軸下　→　10　" << std::endl;
 	int pin;
 	std::cin >> pin;
 
@@ -29,8 +31,3 @@ int main(){
 	}
 	return 0;
 }
-<<<<<<< HEAD
-
-
-=======
->>>>>>> b485cda439b7035f68415b78a71e8d5320503c22
