0c86c57c8c26e1c9c7901a3ac7e10e6c91256f8c
yootee <yooteerow@yahoo.co.jp>
2019-08-23 19:24:46 +0900

    新機能であそんでみた

--- main.cpp
+++ main.cpp
@@ -21,11 +21,9 @@ int main(void){
 	constexpr short HANGER_LEFT_SOLENOID = 3;
 	constexpr short HANGER_RIGHT_SOLENOID = 3;
 	//constexpr short POWER_WINDOW_MOTOR_NUM = 4;
-	
+
 	double regulation = 0.3;
 
-	bool control_mode_flag = true;
-		
 	controller.update();
 	try{
 		ms.init();
@@ -43,7 +41,7 @@ int main(void){
 	bool box_flag = true;
 
 	bool y_arm_flag = true;
-	
+
 	UPDATELOOP(controller, !(controller.button(RPDS3::START) && controller.button(RPDS3::RIGHT))){
 
 		double left_distance = 0;
@@ -61,12 +59,16 @@ int main(void){
 		double right_x = controller.stick(RPDS3::RIGHT_X) * -1;
 		double right_y = controller.stick(RPDS3::RIGHT_Y) ;
 		left_distance = std::sqrt(std::pow(left_x,2) + std::pow(left_y,2)) * 2;
+
+		double sum_turn = controller.stick(RPDS3::RIGHT_T) - controller.stick(RPDS3::LEFT_T);
+
 		double right_front_motor_pwm;
 		double right_back_motor_pwm;
 		double left_front_motor_pwm;
 		double left_back_motor_pwm;
 		double accelaration = 1;
-		if(controller, (controller.button(RPDS3::UP) || controller.button(RPDS3::DOWN)) || (controller.button(RPDS3::RIGHT) || controller.button(RPDS3::LEFT))){
+		
+		if(sum_turn == 0){
 			if(controller.button(RPDS3::UP)){
 				if((right_front_motor_pwm == 0 && right_back_motor_pwm == 0) && (left_front_motor_pwm == 0 && left_back_motor_pwm == 0)){
 					right_front_motor_pwm = -10;
@@ -142,63 +144,17 @@ int main(void){
 				left_front_motor_pwm = 0;
 				left_back_motor_pwm = 0;
 			}
-
-			ms.send(UNDERCARRIAGE_MDD_NUM,RIGHT_FRONT_MOTOR_NUM,right_front_motor_pwm*regulation);
-			ms.send(UNDERCARRIAGE_MDD_NUM,RIGHT_BACK_MOTOR_NUM,right_back_motor_pwm*regulation);
-			ms.send(UNDERCARRIAGE_MDD_NUM,LEFT_FRONT_MOTOR_NUM,left_front_motor_pwm*regulation);
-			ms.send(UNDERCARRIAGE_MDD_NUM,LEFT_BACK_MOTOR_NUM,left_back_motor_pwm*regulation);
-
 		}else{
-			if(control_mode_flag == true){
-
-				if(left_distance > PWM_MAX_VALUE){
-					left_distance = PWM_MAX_VALUE;
-				}
-
-
-				left_theta = std::atan2(-left_y,left_x) + M_PI;
-
-				if(left_theta >= 0 && left_theta <= (M_PI/2)){
-					left_back = (left_theta * 4 / M_PI) - 1;
-					left_front = 1;
-				}else if(left_theta > (M_PI / 2) && left_theta <= (M_PI)){
-					left_back = 1;
-					left_front = -(left_theta * 4/M_PI) + 3;
-				}else if(left_theta > (M_PI) && left_theta <= (3* M_PI /2)){
-					left_back = -(left_theta * 4 / M_PI) + 5;
-					left_front = -1;
-				}else if(left_theta > (3* M_PI /2) && left_theta <= (2* M_PI )){
-					left_back = -1;
-					left_front = (left_theta * 4 / M_PI ) - 7;
-				}
-
-
-				revolve = (controller.stick(RPDS3::RIGHT_T) - controller.stick(RPDS3::LEFT_T))*0.3;
-
-				//std::cout << -left_distance < "::" << left_distance << std::endl;
-				std::cout << left_front <<std::endl;
-
-				ms.send(UNDERCARRIAGE_MDD_NUM, LEFT_FRONT_MOTOR_NUM, -left_distance * left_front  * 0.4 * regulation + revolve);//左前
-				ms.send(UNDERCARRIAGE_MDD_NUM, LEFT_BACK_MOTOR_NUM,  -left_distance * left_back  * 0.4 * regulation + revolve);//左後
-				ms.send(UNDERCARRIAGE_MDD_NUM, RIGHT_FRONT_MOTOR_NUM, left_distance * left_back  * 0.4 * regulation + revolve);//右前
-				ms.send(UNDERCARRIAGE_MDD_NUM, RIGHT_BACK_MOTOR_NUM,  left_distance * left_front * 0.4 * regulation + revolve);//右後 
-			}else{
-				ms.send(UNDERCARRIAGE_MDD_NUM, LEFT_FRONT_MOTOR_NUM,  -left_y * 0.5 * regulation);
-				ms.send(UNDERCARRIAGE_MDD_NUM, LEFT_BACK_MOTOR_NUM,   -left_y * 0.5 * regulation);
-				ms.send(UNDERCARRIAGE_MDD_NUM, RIGHT_FRONT_MOTOR_NUM, right_y * 0.5 * regulation);
-				ms.send(UNDERCARRIAGE_MDD_NUM, RIGHT_BACK_MOTOR_NUM,  right_y * 0.5 * regulation);
-
-			}
-	
+			right_front_motor_pwm = sum_turn;
+                        right_back_motor_pwm = sum_turn;
+                        left_front_motor_pwm = sum_turn;
+                        left_back_motor_pwm = sum_turn;
 		}
-		if(controller.press(RPDS3::R1) == true && controller.press(RPDS3::L1) == true){
-			if(control_mode_flag == true){
-				control_mode_flag = false;
-			}else{
-				control_mode_flag = true;
-				std::cout << "mode" << std::endl;
-			}
-		}
+		ms.send(UNDERCARRIAGE_MDD_NUM,RIGHT_FRONT_MOTOR_NUM,right_front_motor_pwm*regulation);
+		ms.send(UNDERCARRIAGE_MDD_NUM,RIGHT_BACK_MOTOR_NUM,right_back_motor_pwm*regulation);
+		ms.send(UNDERCARRIAGE_MDD_NUM,LEFT_FRONT_MOTOR_NUM,left_front_motor_pwm*regulation);
+		ms.send(UNDERCARRIAGE_MDD_NUM,LEFT_BACK_MOTOR_NUM,left_back_motor_pwm*regulation);
+
 
 		if(controller.press(RPDS3::SQUARE)){
 			if(right_hanger_flag == true){
@@ -246,8 +202,8 @@ int main(void){
 				ms.send(MECHANISM_MDD_NUM,BOX,0);
 				box_flag == true	
 			}
-		
-				
+
+
 		}
 		//else{
 		//	ms.send(MECHANISM_MDD_NUM,BOX,0);
